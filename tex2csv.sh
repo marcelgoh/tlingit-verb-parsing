9837239d5b3a86780871b3511bdbe961cb1c6c69
Marcel Goh <marcel.goh@mail.mcgill.ca>
2021-02-06 16:45:25 -0500

    Added CSV maker

---
+++ tex2csv.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+# Create CSV data from $1.tex and places it in $1.csv (overwriting whatever was there)
+# Written by Marcel Goh on 24 January 2021
+
+awk 'BEGIN { read = 0; skip = 0; }\
+    $1 == "\\midrule" { read = 1; print ""; next }\
+    $1 == "\\bottomrule" { read = 0; skip=1-skip }\
+    read == 1 && skip == 0 { print $0 }' $1.tex | # delete everything except lines ...
+    # ... that are between EVERY OTHER \midrule and \bottomrule pair
+sed 's/&/,/g' | # replace '&' characters with commas (thereby converting to csv)
+tr -d "[:blank:]" | # delete extra horizontal whitespace
+sed $'s/\\\\//g' | # delete trailing '\\'
+sed 's/-//g' | # delete dashes
+sed 's/rlap{}//g' | # delete "right overlap" macro
+awk 'BEGIN { FS=IFS="," }\
+    {for (i=1;i<=NF;++i) {\
+        if (substr($i, 1, 1) == "?") { printf "%s,",substr($i, 3, length($i)-3); }\
+        else { printf "%s,", $i; }\
+    } printf "\n";}' > $1.csv # delete ?{ and the corresponding }
+
+
